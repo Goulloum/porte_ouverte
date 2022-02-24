2ee9f0ae54a5700f9db712e5658b2c5f9876baed
Goulloum <math.guillemin@hotmail.fr>
2022-02-24 16:25:24 +0100

    delete info 2

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,6 @@ function Home(){
         <div className="home-container">
             <div className="main-title">Bienvenue à l'ISCIO</div>
             <div className="link-container">
-                <Link to='/Information'><div className="info-btn">Informations</div></Link>
                 <Link to ='/Plan'><div className="plan-btn">Plan</div></Link>
                 <Link to='/Enregistrer'><div className="enregistrer-btn">S'enregistrer</div></Link>
             </div>
